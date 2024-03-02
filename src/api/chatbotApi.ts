966057c4596f71571c94c7aacc8cb23e2de9b8c1
GrayPham <hunghtk456@gmail.com>
2024-03-02 15:13:41 +0700

    feat/chatbot_02

--- src/api/chatbotApi.ts
+++ src/api/chatbotApi.ts
@@ -1,26 +1,25 @@
 export const chatbotApi = async (userMessage: string) => {
-  const API_URL = "https://api.openai.com/v1/chat/completions";
-
-  try {
-      const requestOptions = {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json",
-              "Authorization": `Bearer YOUR_OPENAI_API_KEY` // Replace with your OpenAI API key
-          },
-          body: JSON.stringify({
-              model: "gpt-3.5-turbo",
-              messages: [{ role: "user", content: userMessage }],
-          })
-      };
-
-      const response = await fetch(API_URL, requestOptions);
-      const data = await response.json();
-      const botResponse = data.choices[0].message.content.trim();
-
-      return botResponse;
-  } catch (error) {
-      console.error("Error fetching response from OpenAI:", error);
-      return "Oops! Something went wrong. Please try again.";
-  }
-};
+    const API_URL = "https://backend-stonkverse.vercel.app/api/v1/chat/ask";
+  
+    try {
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Connection": "keep-alive"
+            },
+            body: JSON.stringify({
+                newMessage: userMessage ,
+            })
+        };
+  
+        const response = await fetch(API_URL, requestOptions);
+        const data = await response.json();
+        const botResponse = data.output.trim();
+  
+        return botResponse;
+    } catch (error) {
+        console.error("Error fetching response from OpenAI:", error);
+        return "Oops! Something went wrong. Please try again.";
+    }
+  };
